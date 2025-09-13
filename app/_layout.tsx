4f3318aff8a4137fcf1bd9f145d9e49198106141
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-09-14 00:33:13 +0530

    feat: created notifications screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,19 @@
-import ClearkAndConvexProvider from "@/providers/ClearkAndConvexProvider";
-import { Stack } from "expo-router";
+import InitialLayout from "@/components/InitialLayout";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+import ClearkAndConvexProvider from "@/providers/ClearkAndConvexProvider";
+import { StatusBar } from "expo-status-bar";
+
 export default function RootLayout() {
+
   return (
     <ClearkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
-          <Stack screenOptions={{ headerShown: false }}></Stack>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }} >
+          <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
+      <StatusBar style="light" />
     </ClearkAndConvexProvider>
-  )
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,5 @@
-import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
-import { Text } from "react-native";
 
 export default function Index() {
-    const { isLoaded, isSignedIn } = useAuth()
-
-    if (!isLoaded) {
-        return <Text>Loading...</Text>
-    }
-
-    if (isLoaded && !isSignedIn) {
-        console.log("is signed in or not", isSignedIn)
-        return <Redirect href={'/(auth)/login'} />
-    }
-
-    return <Redirect href="/(tabs)" />
+    return <Redirect href="/(auth)/login" />;
 }
